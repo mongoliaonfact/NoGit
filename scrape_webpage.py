fe6d38ad5cd1f292b036c5186a253c64609f6fb1
mongoliaonfact <78066728+mongoliaonfact@users.noreply.github.com>
2022-11-29 08:37:31 -0500

    new changes

--- scrape_webpage.py
+++ scrape_webpage.py
@@ -6,6 +6,10 @@
 from bs4 import BeautifulSoup
 import requests
 from pandas import DataFrame, read_csv
+from collections import Counter
+from danielkennet import tech2
+from datetime import datetime 
+
 
 def single_transcript(url):
     text_content = []
@@ -14,12 +18,44 @@ def single_transcript(url):
     text = [''.join(s.findAll(text=True)) for s in soup.findAll('p')]
     for line in text:
         text_content.append(line)
-    #with open(f'{filename}.txt', 'wt', encoding='utf-8') as newfile:
-        #newfile.write(text[0])
-    #    for line in text[0]:
-    #        newfile.write(line+" \n")
-            #newfile.write('\n')
+
+def single_transcript_v2(url):
+    
+    breaks = ['Question-and-Answer', 'Questions-and-Answers', 'Question-and-Answer Session', 'Question-and-Answer Session ',
+              'And with that, we would like to open up the call for Q&A. Operator?', 'Question-And-Answer Session ',
+              'Question-And-Answer Session', 'Questions-and-Answers Session', 'Questions-and-Answer Session']
+    text_content = []
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    text = [''.join(s.findAll(text=True)) for s in soup.findAll('p')]
+    company_script = []
+    for i, line in enumerate(text):
+        company_script.append(line)
+        if line in breaks:
+            ind = i
+        #print(f'index: {i}, {[line]}')
+    #print(f'break: {ind}')
+    #print('-------------------------------------')  
+    #print(company_script) 
+    #print('=====================================')
+    #print(company_script[: ind])
+    
+    text_content.append({
+        'presentation': company_script[: ind],
+        'q_and_a': company_script[ind +1: ] 
+    })
     return text_content
+
+def write_transcript(url, filename):    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    text = [''.join(s.findAll(text=True)) for s in soup.findAll('p')]
+    with open(f'{filename}.txt', 'wt', encoding='utf-8') as newfile:
+        for line in text:
+            newfile.writelines(line)
+            #newfile.write('\n')
+
+
     
 def get_transcript_info(a_script):
     '''
@@ -59,27 +95,69 @@ def get_transcript_info(a_script):
     
     return info
 
+def either(x):
+    y = ''
+    if x in tech2: y = 'yes'
+    else: y = 'no'
+    return y
+
 def main():
-    
-    url = 'https://seekingalpha.com/article/4560263-golub-capital-bdc-gbdc-q4-2022-earnings-call-transcript'
+    start = datetime.now()
+    #url = 'https://seekingalpha.com/article/4560263-golub-capital-bdc-gbdc-q4-2022-earnings-call-transcript'
     #url = 'https://seekingalpha.com/article/4560276-aft-pharmaceuticals-limited-aftpf-q2-2023-earnings-call-transcript'
     #url = 'https://seekingalpha.com/article/4560281-naspers-ltd-and-prosus-nv-prosy-q2-2022-earnings-call-transcript' 
+    #url = 'https://seekingalpha.com/article/4380147-duck-creek-technologies-inc-dct-ceo-mike-jackowski-on-q4-2020-results-earnings-call'
+    #url = 'https://seekingalpha.com/article/4546251-duck-creek-technologies-inc-dct-q4-2022-earnings-call-transcript'
+    #url = 'https://seekingalpha.com/article/4367179-ceva-inc-ceva-ceo-gideon-wertheizer-on-q2-2020-results-earnings-call-transcript'
+    #url = 'https://seekingalpha.com/article/4362686-2u-inc-twou-ceo-chip-paucek-on-q2-2020-results-earnings-call-transcript'
+    #url = 'https://seekingalpha.com/article/4298925-sps-commerce-inc-spsc-ceo-archie-black-on-q3-2019-results-earnings-call-transcript'
+    #url = 'https://seekingalpha.com/article/4481454-commvault-systems-inc-cvlt-ceo-sanjay-mirchandani-on-q3-2022-results-earnings-call-transcript'
+    #url = 'https://seekingalpha.com/article/4261266-cornerstone-ondemand-inc-csod-ceo-adam-miller-on-q1-2019-results-earnings-call-transcript'
+    '''
+    breaks = ['Question-and-Answer', 'Questions-and-Answers', 'Question-and-Answer Session', 'Question-and-Answer Session ', 
+              'And with that, we would like to open up the call for Q&A. Operator?', 'Question-And-Answer Session ', 
+              'Question-And-Answer Session', 'Questions-and-Answers Session', 'Questions-and-Answer Session']
+    text_content = []
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    text = [''.join(s.findAll(text=True)) for s in soup.findAll('p')]
+    company_script = []
+    for i, line in enumerate(text):
+        company_script.append(line)
+        if line in breaks:
+            ind = i
+        print(f'index: {i}, {[line]}')
+    print(f'break: {ind}')
+    #print('-------------------------------------')  
+    #print(company_script) 
+    #print('=====================================')
+    #print(company_script[: ind])
     
-    #a_script = single_transcript(url)
-    #company = get_transcript_info(a_script)
-    #print(company[0]['company_info'][0])
-    #print(company[0]['from_company'])
-    #print(company[0]['others'])
-    #print(company[0]['q_and_a'])
+    #a_script = single_transcript_v2(url)
+    #print(DataFrame(a_script))
     
+    #print(tech2)
+    '''
+    df = read_csv('main_tbl.csv')
+    print(df)
+    df['prep'] = df['tick'].apply(lambda x: either(x))
+    daniel = df[df['prep'] == 'yes'].loc[:, ['names', 'url', 'tick']]
+    for ind, row in daniel.iterrows():
+        name = row['names']
+        tick = row['tick']
+        url = row['url']
+        #script = single_transcript_v2(url)
+        try:
+            script = single_transcript_v2(url)
+            print(name, tick, script)
+        except UnboundLocalError:
+            print('--------------------------')
+            print(f'this is error message {url}')
+            return #'''
+    end = datetime.now()
     
-    #filename = open('first.txt', 'w', encoding='utf-8')
-    #filename.write(company)
-    #filename.close()
-    #df = DataFrame(company)
-    #df.to_csv('company.csv', index=False)
-    
-    print(read_csv('company.csv'))
+    print(f'Total time is: {end - start}')
+
     
 if __name__ == '__main__':
     main()
